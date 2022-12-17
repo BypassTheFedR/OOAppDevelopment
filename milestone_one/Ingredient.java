e136c044c6e49c7992c22b60b42f519499a7bf45
BypassTheFedR <joshuarthornburg@gmail.com>
2022-12-17 13:59:27 -0700

    Changed to capital I

--- milestone_one/Ingredient.java
+++ milestone_one/Ingredient.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 
-public class ingredient {
+public class Ingredient {
     
     public static void main(String[] args) {
         String nameOfIngredient = "";
